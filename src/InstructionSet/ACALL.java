60d89fb11041b368693e3c267377a99b6d839f77
Pätito <66649091+p4tit0@users.noreply.github.com>
2022-01-10 12:12:52 -0300

    a

--- src/InstructionSet/ACALL.java
+++ src/InstructionSet/ACALL.java
@@ -22,9 +22,9 @@ public class ACALL extends Instruction{
         
         Cpu.PC += 2;                      //PC += 2
         Memory.ram[0x81]++;               // SP++
-        Memory.ram[Memory.ram[0x81]] = Cpu.PC & 0xff;// (SP) = PC[7-0]
+        Memory.ram[Memory.ram[0x81]] = (Cpu.PC & 0xf0) >> 4;// (SP) = PC[7-0]
         Memory.ram[0x81]++;               // SP++
-        Memory.ram[Memory.ram[0x81]] = (Cpu.PC>>8) & 0xff;// (SP) = PC[15-8]
+        Memory.ram[Memory.ram[0x81]] = Cpu.PC & 0x0f;// (SP) = PC[15-8]
                     //?? PC10-0 = A10-0
     }
 }
